82cde26546fbf64fe54f14174c8de997b451a9dc
Luis Mendes <luismendes535@gmail.com>
2018-04-25 15:36:53 +0100

    env.port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', (req,res)=>{
     res.send({Hi:'There'})
 });
 
-app.listen(5000,()=>{
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
     console.log('Server running on port 5000')
 });
