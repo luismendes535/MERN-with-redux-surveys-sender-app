0c9363702efb03ee905c86bdcad16992816bd02e
Luis Mendes <luismendes535@gmail.com>
2018-05-10 19:49:35 +0100

    Users can see surveys results

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+  { label: " Survey Title", name: "title" },
+  { label: "Subject line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipients List", name: "recipients" }
+];
